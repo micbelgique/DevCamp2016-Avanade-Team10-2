cfab9ba8e6b7c4e8d25a472dad5bd33b20447dff
Richard Christopher <0x205m94@hotmail.be>
2016-08-06 11:14:32 +0200

    Init Frontend

---
+++ Zugma-Frontend/Assets/Scripts/Initialization.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Net;
+
+public class Initialization : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
